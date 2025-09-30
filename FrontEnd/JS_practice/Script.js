ec6ddb972e9b20cca717b3e32db93820adf92985
Rajnish-chauhan <crajnish425@gmail.com>
2025-10-01 00:00:02 +0530

    Js Practice window

--- FrontEnd/JS_practice/Script.js
+++ FrontEnd/JS_practice/Script.js
@@ -1,3 +1,88 @@
-for(let i=0;i<=5;i++){
-    console.log("Hello coders");
-}
+//  ^   **************************  LOOPS  **************************************
+
+//  ! for loop
+/*
+
+    for(let i=0;i<=5;i++){
+        console.log("Hello coders");
+    }    
+
+*/
+
+
+// !   while loop
+/*
+    let i=1;
+    while(i<=5){
+        console.log(i);
+        i++;
+    }
+*/
+
+
+//  ! do while loop
+/*  let i=1;
+    do{
+        console.log("Hello coders");
+        i++;
+    }while(i<=5);   */
+
+
+//  ! For-of loop
+
+//   let str="JavaScript";
+
+  /*  for(let i of str){
+        console.log("i=",i);
+    
+    }         */
+  
+
+
+// !  For in Loop
+/*
+    const Student={
+        name:"Rajnish",
+        Course:"Btech",
+        tech_stack:"Java Developer"
+    };
+
+    for(let key in Student)
+    { 
+        console.log(Student);   // ! its print All exact value on according to data means how many data its pront those times
+        console.log(key);    //  !  only print keys
+        console.log(key,Student[key]);  // ! print data and values
+        
+    }     
+*/
+
+
+
+
+// ! For of loop
+/*
+    const Name=['a','p','n','a'];
+    const str="Rajnish";
+    for(let val of str){
+        console.log(val);
+        console.log(val);
+        
+        
+    }         */
+
+// !  forEach method
+
+// ^  It's a method not loop
+/*
+    const arr=[1,2,3];
+    arr.forEach((value,index)=>{
+        console.log("Index No:"+index+", Value:",value);
+        
+    });  */
+
+    
+
+//  ^  *************************************************   LOOPS  END  ****************************************
+
+
+console.log("Hello")
